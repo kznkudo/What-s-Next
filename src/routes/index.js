1f13b77ef159d29747c92acceae4144766abbef1
Oliver Eisfeld <eisfeldoliver@gmail.com>
2025-07-03 14:05:58 +0200

    register hinzufügen, design slightly überarbeiten

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,25 @@
+import { name } from 'ejs';
 import express from 'express';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: "What's Next" });
+    res.render('index', { hideLayout: true, title: "What's Next" });
 });
 router.get('/login', (req, res) => {
-    res.render('login', { title: "What's Next" });
+    res.render('login', { hideLayout: false, title: "What's Next" });
+});
+router.get('/register', (req, res) => {
+    res.render('register', { hideLayout: false, title: "What's Next" });
 });
 router.get('/dashboard', (req, res) => {
-    res.render('dashboard', { title: "What's Next" });
+    res.render('dashboard', { hideLayout: false, title: "What's Next" });
+});
+router.post('/login', (req, res)=>{
+  const data ={name, }
+})
+router.get('/AGBs', (req, res) => {
+    res.render('html/AGBs', { hideLayout: true, title: "What's Next" });
 });
 export default router;
 
